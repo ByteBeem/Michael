cac1414c9fda1808599e3b0755be99d8a933f16a
Bytebeem <donald.mxolisi@proton.me>
2024-10-08 20:59:42 +0200

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
         <span class="text-white">&copy; 2024 Copyright - </span>
-        <a class="link-underline text-white font-bold" href="https://mr-Michael.netlify.app">Wendy Musumuvhi</a>
+        <a class="link-underline text-white font-bold" href="https://mr-Michael.netlify.app">Michael</a>
       </div>
     )
 }
